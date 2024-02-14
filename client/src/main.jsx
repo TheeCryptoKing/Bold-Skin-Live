86a2f08c1aa328f2f2894de34ce0d80484cef401
Evan <eroberts1998@icloud.com>
2024-02-14 00:52:26 -0500

    remove stylesheet

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './stylesheet/index.css'
 // Not to be touched change css
 
 ReactDOM.createRoot(document.getElementById('root')).render(
